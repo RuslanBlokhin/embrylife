f490d46bd134c207dc51e7d83853392b6359f857
Ruslan Blokhin <blokhin.rus@gmail.com>
2023-03-27 13:09:08 +0300

    header desktop menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,6 +5,19 @@ const mobileMenu = document.getElementById('mobile-menu')
 const servicesBtn = document.getElementById('services-btn')
 const servicesList = document.getElementById('services-list')
 const mobMenuList = document.getElementById('mobile-menu-list')
+const services = document.getElementById('services')
+const menuDesktopBtns = document.querySelectorAll('#open-desktop-menu-btn')
+const body = document.getElementById('body')
+
+body.addEventListener('click', e => {
+    if(!(e.target.classList.contains('header__services-link') || 
+    e.target.classList.contains('header__services') || 
+    e.target.classList.contains('header__nav-arrowDown-btn')) 
+    && services.hidden === false) {
+            removeClass()
+            services.hidden = true
+        }
+})
 
 menuBtn.addEventListener('click', e => {
     mobileMenu.hidden = false
@@ -24,3 +37,28 @@ backBtns.forEach(btn => {
         mobMenuList.hidden = false
     })
 })
+
+menuDesktopBtns.forEach(btn => {
+        btn.addEventListener('click', e => {
+        removeClass()
+        const arrow = e.target.firstElementChild
+        const currentItem = e.target.parentElement
+
+        arrow.classList.add('rotate')
+        currentItem.classList.add('active')
+
+        services.hidden = false
+    })
+})
+
+function removeClass() {
+    menuDesktopBtns.forEach(btn => {
+        const arrow = btn.firstElementChild
+        const currentItem = btn.parentElement
+        
+        if(currentItem.classList.contains('active')) {
+            arrow.classList.remove('rotate')
+            currentItem.classList.remove('active')
+        }
+    })
+}
