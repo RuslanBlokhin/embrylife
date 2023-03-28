21b9e68fc31c5cb3b7226a0d4f2187a7f70f738f
Ruslan Blokhin <blokhin.rus@gmail.com>
2023-03-28 14:13:31 +0300

    add main page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import Glide from '@glidejs/glide'
 
 const glide = new Glide('.glide', {
-    type: 'carousel'
+    type: 'carousel',
+    startAt: 2
+}).mount()
+
+new Glide('.main__request-gallery', {
+    // type: 'slider',
+    // focusAt: 'center',
+    // peek: {
+    //     after: '100'
+    // },
+    gap: 30,
+    perView: 4,
+    breakpoints: {
+        1599: {
+            gap: 10,
+            perView: 1
+        }
+    }
 }).mount()
